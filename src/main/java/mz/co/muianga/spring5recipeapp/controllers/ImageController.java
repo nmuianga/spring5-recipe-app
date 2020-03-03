f7a0dbe9be14171158df54502cff60cc1625fbb7
Nilvandro Muianga <nbcmuianga@gmail.com>
2020-03-03 15:45:46 +0200

    Uploading Image (uncompleted)

---
+++ src/main/java/mz/co/muianga/spring5recipeapp/controllers/ImageController.java
@@ -0,0 +1,11 @@
+package mz.co.muianga.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Nilvandro Muianga on 3/3/2020
+ */
+@Controller
+public class ImageController {
+
+}
